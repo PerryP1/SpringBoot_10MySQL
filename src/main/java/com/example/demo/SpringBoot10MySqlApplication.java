fb586e95d0b21f4117a87662a141b300b9df7e47
PerryP1 <perryapublico@gmail.com>
2018-08-10 16:23:26 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBoot10MySqlApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot10MySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot10MySqlApplication.class, args);
+	}
+}
